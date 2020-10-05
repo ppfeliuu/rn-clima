f5ebfbbd3c9e0788cbdb01dcefae3d11b9b1ee1c
Pedro Feliu <pedro@medanosol.es>
2020-10-05 12:56:22 +0100

    first commit

---
+++ components/Formulario.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Picker } from "@react-native-community/picker";
+const Formulario = () => {
+  return (
+    <>
+      <View style={styles.formulario}>
+        <View>
+          <TextInput placeholder="Ciudad" placeholderTextColor="#666" />
+        </View>
+        <View>
+          <Picker>
+            <Picker.Item label="-Selecciones un país-" value="" />
+            <Picker.Item label="EE.UU." value="US" />
+            <Picker.Item label="México" value="MX" />
+            <Picker.Item label="Argentina" value="AR" />
+            <Picker.Item label="Colombia" value="CO" />
+            <Picker.Item label="Costa Rica" value="CR" />
+            <Picker.Item label="España" value="ES" />
+            <Picker.Item label="Perú" value="PE" />
+          </Picker>
+        </View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  formulario: {
+    marginTop: 100,
+  },
+});
+
+export default Formulario;
